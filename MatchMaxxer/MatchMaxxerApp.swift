ec23d5892544352776df42941accc65add8537be
cmillstein1 <cmillstein1@me.com>
2026-05-02 16:01:49 -0400

    Initial Commit

---
+++ MatchMaxxer/MatchMaxxerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMaxxerApp.swift
+//  MatchMaxxer
+//
+//  Created by Casey Millstein on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MatchMaxxerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
